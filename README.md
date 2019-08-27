895e075daf7050d7c7ab49a71f91b5529f861c33
Tong Wu <tongwu.github@outlook.com>
2019-08-28 07:57:17 +0800

    add python sample

--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 |   | Array | List | Map | Set |
 | ------------- | ------------- | ------------- | ------------- | ------------- |
+| Init  | O | O | O | O |
 | Index  | O | O | X | X |
-| Sort   | O | O | X | X |
+| Length | O | O | O | O |
+| Sort(desc) | O | O | X | X |
+| OrderBy    | O | O | X | X |
 | Convert| O | O | O | O |
-| Add    | X | O | O | O |
-| AppendRange | X | O | O | O |
+| Insert(Add)| X | O | O | O |
+| Append(Range) | X | O | X | O |
 | Remove | X | O | O | O |
-| Merge  | X | O | O | O |
 | Traverse   | O | O | O | O |
 | Contains   | O | O | O | O |
+| Shuffle    | O | O | X | X |
+| Filter     | X | O | O | O |
+
---
+++ sample.py
@@ -0,0 +1,49 @@
+# List
+inputs = [0] * 10
+inputs[5] += 1
+print(len(inputs))
+inputs.sort(reverse=True)
+print(inputs)
+inputs = sorted(inputs, key=lambda i: -i + 1)
+print(inputs)
+inputs = [i * 2 for i in inputs]
+print(inputs)
+inputs.insert(3, 10) #index is 3, value is 10
+print(inputs)
+inputs.append(3)
+inputs.extend([4, 5])
+print(inputs)
+inputs.remove(5) # 5 is value
+inputs.pop()
+inputs.pop(3) #3 is index
+print(inputs)
+for i, v in enumerate(inputs):
+    print('{0} {1}'.format(i, v))
+if 2 in inputs:
+    print(True)
+from random import shuffle
+shuffle(inputs)
+
+inputs = list(filter(lambda i: i > 0, inputs))
+print(inputs)
+print(sum(inputs))
+
+#Map
+inputs = {4:5}
+inputs[1] = 3
+inputs[1] += 1
+print(len(inputs))
+keys = [k for k in inputs.keys()]
+values = [v for v in inputs.values()]
+sums = [v+k for k, v in inputs.items()]
+print(keys)
+print(values)
+print(sums)
+inputs.pop(1)
+print(inputs)
+inputs[2] = 3
+inputs = {k:k for k in range(10)}
+print(inputs)
+
+inputs = {k: v for k, v in inputs.items() if k > 5 and v < 7}
+print(inputs)
